9ac7a11a0d10684246dd6a4a68636fbe7d066195
ProjectJYL <jianyet.lee@gmail.com>
2016-09-14 18:02:46 -0400

    Add files via upload

---
+++ Arduino/sketch_jun08a/sketch_jun08a.ino
@@ -0,0 +1,121 @@
+//---------------------------------WAVE---------------------------------------------------------
+#include <WaveHC.h>
+#include <WaveUtil.h>
+
+SdReader card;    // This object holds the information for the card
+FatVolume vol;    // This holds the information for the partition on the card
+FatReader root;   // This holds the information for the volumes root directory
+WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+
+/*
+ * Define macro to put error messages in flash memory
+ */
+#define error(msg) error_P(PSTR(msg))
+
+// Function definitions (we define them here, but the code is below)
+void play(FatReader &dir);
+//---------------------------------WAVE---------------------------------------------------------
+
+//---------------------------------PIR---------------------------------------------------------
+const int MOTION_PIN = 6; // Pin connected to motion detector
+const int LED_PIN = 13; // LED pin - active-high
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);           // set up Serial library at 9600 bps for debugging
+  
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)  
+    error("Card init. failed!");  // Something went wrong, lets print out why
+  }
+
+  // Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {   // we have up to 5 slots to look in
+    if (vol.init(card, part)) 
+      break;                           // we found one, lets bail
+  }
+  if (part == 5) {                     // if we ended up not finding one  :(
+    error("No valid FAT partition!");  // Something went wrong, lets print out why
+  }
+
+    // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    error("Can't open root dir!");      // Something went wrong,
+  }
+
+  // The PIR sensor's output signal is an open-collector, 
+  // so a pull-up resistor is required:
+  pinMode(MOTION_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int proximity = digitalRead(MOTION_PIN);
+  if (proximity == LOW) // If the sensor's output goes low, motion is detected
+  {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Motion detected!");
+    //play sound! 
+    root.rewind();
+    play(root, "sound1.wav"); 
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }  
+}
+
+/////////////////////////////////// HELPERS
+/*
+ * print error message and halt
+ */
+void error_P(const char *str) {
+  PgmPrint("Error: ");
+  SerialPrint_P(str);
+  sdErrorCheck();
+  while(1);
+}
+/*
+ * print error message and halt if SD I/O error, great for debugging!
+ */
+void sdErrorCheck(void) {
+  if (!card.errorCode()) return;
+  PgmPrint("\r\nSD I/O error: ");
+  Serial.print(card.errorCode(), HEX);
+  PgmPrint(", ");
+  Serial.println(card.errorData(), HEX);
+  while(1);
+}
+
+void play(FatReader &dir, char* filename)
+{
+  FatReader file;
+
+  if (!file.open(dir, filename)) {        // open the file in the directory
+    error("file.open failed");          // something went wrong
+  }
+  else 
+  {
+      // Aha! we found a file that isnt a directory
+      putstring("Playing ");
+      Serial.print(filename);              // print it out
+      if (!wave.create(file)) {            // Figure out, is it a WAV proper?
+        putstring(" Not a valid WAV");     // ok skip it
+      } else {
+        Serial.println();                  // Hooray it IS a WAV proper!
+        wave.play();                       // make some noise!
+        
+        uint8_t n = 0;
+        while (wave.isplaying) {// playing occurs in interrupts, so we print dots in realtime
+          putstring(".");
+          if (!(++n % 32))Serial.println();
+          delay(100);
+        }       
+        sdErrorCheck();                    // everything OK?
+        // if (wave.errors)Serial.println(wave.errors);     // wave decoding errors
+      }
+    }
+}
+
+
